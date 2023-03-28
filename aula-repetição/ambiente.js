956622a4b048c2d6803579e06a670f1b0593ac38
mateussuzuki <mateussuzuki2022@hotmail.com>
2023-03-27 23:22:52 -0300

    repetição

---
+++ aula-repetição/ambiente.js
@@ -0,0 +1,28 @@
+
+
+
+/*for(var c = 1; c <= 9; c++) {
+    console.log(c)
+}
+    FOR 
+*/
+
+
+
+
+/*var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+    DO 
+*/
+
+
+/*var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+    WHILE
+ */
--- teste/script.js
+++ teste/script.js
@@ -3,10 +3,10 @@ function verificar() {
     var ano = data.getFullYear()
     var iano = document.querySelector("input#iano")
     var res = document.querySelector("div#res")
-    if (iano.value.lenght == 0 || iano.value > ano) {
-        window.alert("ERRO essa idade não existe")
+    if (iano.value.length == 0 || Number(iano.value) > ano) {
+        window.alert("[ERRO] Verifique os dados e tente novamente")
     } else {
-        var idade = ano - Number(ano.value)
+        var idade = ano - Number(iano.value)
         var sex = document.querySelector("input.sex")
         var img = document.createElement("img")
         var genero = ""
@@ -14,23 +14,36 @@ function verificar() {
             genero ="Homem"
             if (idade >= 0 && idade < 10) {
                 //criança
-                img.appendChild("src", "criança-menino.png")
+                img.setAttribute("src", "criança-menino.png")
             } else if (idade < 20) {
                 //jovem
-                img.appendChild("src", "jovem-menino.png")
+                img.setAttribute("src", "jovem-menino.png")
             } else if (idade < 35) {
                 //adulto
-                img.appendChild("src", "adulto-menino.png")
+                img.setAttribute("src", "adulto-menino.png")
             }else if (idade < 50) {
                 //idoso
-                img.appendChild("src", "idoso-menino.png")
+                img.setAttribute("src", "idoso-menino.png")
             }
         } else {
             genero = "Mulher"
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute("src", "criança-menina.png")
+            } else if (idade < 20) {
+                //jovem
+                img.setAttribute("src", "jovem-menina.png")
+            } else if (idade < 35) {
+                //adulto
+                img.setAttribute("src", "adulto-menina.png")
+            }else if (idade < 50) {
+                //idoso
+                img.setAttribute("src", "idoso-menina.png")
+            }
         }
         res.style.textAlign = "center"
-        res.innerHTML = `${genero}`
-        
+        res.innerHTML = `É um ${genero} com ${idade} anos!`
+        res.appendChild(img)
         
     }
 }
