10654cc701ed38910d3d7f67211dc06d1c5d566d
mateussuzuki <mateussuzuki2022@hotmail.com>
2023-04-20 18:13:16 -0300

    ex

--- exercício-analisador/script.js
+++ exercício-analisador/script.js
@@ -1,12 +1,25 @@
 
+let arr = []
 function adicionar() {
     let num = document.querySelector("#number-num").value
-    var select = document.querySelector("#select-numeros")
+    let select = document.querySelector("#select-numeros")
     if (num == 0 || num > 100) {
         window.alert("O número não foi encontrado ou já foi adicionado")
     } else {
         let sec = document.createElement("option")
             sec.innerHTML = `O valor ${num} foi adicionado`
             select.appendChild(sec)
-    }
+        }
+        let res = []
+        arr.push(num)
+        arr.sort()
+    }      
+
+function finalizar() {
+    return arr[arr.length - 1]
+    let p = document.createElement("p")
+    p.innerHTML = `O valor ${num} foi adicionado`
+    select.appendChild(sec)
+
+
 }
