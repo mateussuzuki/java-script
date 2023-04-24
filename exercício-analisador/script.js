ec9541e38f59806b69359c6b192ecb9bb322ad01
mateussuzuki <mateussuzuki2022@hotmail.com>
2023-04-24 13:21:17 -0300

    asds

--- exercício-analisador/script.js
+++ exercício-analisador/script.js
@@ -1,25 +1,26 @@
 
 let arr = []
+let maiornumerolista
 function adicionar() {
     let num = document.querySelector("#number-num").value
-    let select = document.querySelector("#select-numeros")
-    if (num == 0 || num > 100) {
+    let select = document.querySelector("#tabela")
+    if (num == 0 || num > 100 || arr.some(item => item == num)) {
         window.alert("O número não foi encontrado ou já foi adicionado")
+        return 
     } else {
-        let sec = document.createElement("option")
+        let sec = document.createElement("div")
             sec.innerHTML = `O valor ${num} foi adicionado`
             select.appendChild(sec)
         }
         let res = []
         arr.push(num)
-        arr.sort()
     }      
 
-function finalizar() {
-    return arr[arr.length - 1]
-    let p = document.createElement("p")
-    p.innerHTML = `O valor ${num} foi adicionado`
-    select.appendChild(sec)
-
-
-}
+    function finalizar() {
+        arr.forEach(item)
+            if(arr.every(item2 => item <= item2)) {
+                let maiornumerolista = item
+            }
+        
+    }
+    console.log(maiornumerolista)
